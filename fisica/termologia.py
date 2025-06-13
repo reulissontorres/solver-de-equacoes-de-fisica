ac65a4f1a45672d26e4d53bea7c8bb7fbce483e1
Reulisson Torres <115098329+reulissontorres@users.noreply.github.com>
2025-06-13 23:32:59 +0000

    #1 Template das fórmulas de conversão de temperatura

---
+++ fisica/termologia.py
@@ -0,0 +1,71 @@
+def converter_celsius_para_fahrenheit(celsius: float) -> float:
+    """Converte a temperatura de Celsius para Fahreheint
+
+    Args:
+        celsius (float): Temperatura em graus Celsius (°C)
+
+    Returns:
+        float: Temperatura em graus Fahrenheit (°F)
+    """
+    pass
+
+
+def converter_celsius_para_kelvin(celsius: float) -> float:
+    """Converte a temperatura de Celsius para Kelvin
+
+    Args:
+        celsius (float): temperatura em graus Celsius (°C)
+
+    Returns:
+        float: Temperatura em Kelvin (K)
+    """
+    pass
+
+
+def converter_fahrenheit_para_celcius(fahrenheit: float) -> float:
+    """Converte a temperatura de Fahreheint para Celsius
+
+    Args:
+        fahrenheit (float): Temperatura em graus Fahreheint (°F)
+
+    Returns:
+        float: Temperatura em graus Celsius (°C)
+    """
+    pass
+
+
+def converter_fahrenheit_para_kelvin(fahrenheit: float) -> float:
+    """Converte a temperatura de Fahreheint para Celsius
+
+    Args:
+        fahrenheit (float): temperatura em graus Fahreheint (°F)
+
+    Returns:
+        float: Temperatura em graus Celsius (°C)
+    """
+    pass
+
+
+def converter_kelvin_para_celsius(kelvin: float) -> float:
+    """Converte a temperatura de Kelvin para Celsius
+
+    Args:
+        kelvin (float): temperatura em Kelvin (°K)
+
+    Returns:
+        float: Temperatura em graus Celsius (°C)
+    """
+    pass
+
+
+def converter_kelvin_fahrenheit(kelvin: float) -> float:
+    """Converte a temperatura de Kelvin para Fahreheint
+
+    Args:
+        kelvin (float): temperatura em Kelvin (K)
+
+    Returns:
+        float: Temperatura em graus Fahreheint (°F)
+    """
+    pass
+
